795aba068f4e70950cb6eae21f852236de1101b2
Paulo Coraini <pacscoraini@gmail.com>
2021-07-22 16:56:44 -0300

    crussh

--- Crush/ViewController.swift
+++ Crush/ViewController.swift
@@ -11,7 +11,62 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let pinkView = UIView()
+        pinkView.backgroundColor = .systemPink
+
+//        let blueView = UIView()
+//        blueView.backgroundColor = .systemBlue
+//
+//        let yellowView = UIView()
+//        yellowView.backgroundColor = .systemYellow
+        
+        view.addSubview(pinkView)
+        
+//        pinkView.fill(
+//            top: view.topAnchor,
+//            leading: view.leadingAnchor,
+//            trailing: view.trailingAnchor,
+//            bottom: view.bottomAnchor
+//        )
+        
+        
+        
+        pinkView.fillSuperView(padding: .init(top: 50, left: 50, bottom: 50, right: 50))
+        
+        
+        
+//        view.addSubview(blueView)
+//
+//
+//        pinkView.translatesAutoresizingMaskIntoConstraints = false
+//        pinkView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+//        pinkView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+//        pinkView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+//        pinkView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+//        pinkView.widthAnchor.constraint(equalToConstant: view.bounds.width / 2).isActive = true
+
+
+        //        blueView.translatesAutoresizingMaskIntoConstraints = false
+        //        blueView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        //        blueView.leadingAnchor.constraint(equalTo: pinkView.trailingAnchor).isActive = true
+        //        blueView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        //        blueView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        let horizontalStackView = UIStackView (arrangedSubviews: [pinkView, blueView])
+        horizontalStackView.distribution = .fillEqually
+
+        
+        let stackView: UIStackView = UIStackView(arrangedSubviews: [horizontalStackView, yellowView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        stackView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
     }
 
 
